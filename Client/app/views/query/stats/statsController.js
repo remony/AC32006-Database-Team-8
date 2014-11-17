e316dd6c7be15689eb175cd2477b336d5b3fcab8
remony <sudouglas@dundee.ac.uk>
2014-11-17 18:26:01 +0000

    removed comments and extra angular import

--- Client/app/views/query/stats/statsController.js
+++ Client/app/views/query/stats/statsController.js
@@ -25,12 +25,11 @@ angular.module('app.query.statsController', ['nvd3'])
             }
         };
 
-        /* Chart data */
         $scope.data = [];
 
   $.ajax({
     type: "GET",
-    url: "http://localhost:8888/backend/sales/statistics/number",
+    url: "http://localhost/backend/sales/statistics/number",
     beforeSend: function (xhr) {xhr.setRequestHeader ("Authorization", $cookies.monster_cookie)},
     }).done(function(data){
       console.log(data);
@@ -41,59 +40,5 @@ angular.module('app.query.statsController', ['nvd3'])
       console.log(data);
       $scope.data = data.sales;
     });
-
-  //var chart1 = {};
-  //  chart1.type = "ColumnChart";
-  //  chart1.cssStyle = "height:500px; width:100%;";
-  //  chart1.data = {"cols": [
-  //      {id: "month", label: "Month", type: "string"},
-  //      {id: "laptop-id", label: "Laptop", type: "number"},
-  //      {id: "desktop-id", label: "Desktop", type: "number"},
-  //      {id: "server-id", label: "Server", type: "number"},
-  //      {id: "cost-id", label: "Shipping", type: "number"}
-  //  ], "rows": [
-  //      {c: [
-  //          {v: "January"},
-  //          {v: 19, f: "42 items"},
-  //          {v: 12, f: "Ony 12 items"},
-  //          {v: 7, f: "7 servers"},
-  //          {v: 4}
-  //      ]},
-  //      {c: [
-  //          {v: "February"},
-  //          {v: 13},
-  //          {v: 1, f: "1 unit (Out of stock this month)"},
-  //          {v: 12},
-  //          {v: 2}
-  //      ]},
-  //      {c: [
-  //          {v: "March"},
-  //          {v: 24},
-  //          {v: 0},
-  //          {v: 11},
-  //          {v: 6}
-  //
-  //      ]}
-  //  ]};
-  //
-  //  chart1.options = {
-  //      "title": "Sales per month",
-  //      "isStacked": "true",
-  //      "fill": 20,
-  //      "displayExactValues": true,
-  //      "vAxis": {
-  //          "title": "Sales unit", "gridlines": {"count": 6}
-  //      },
-  //      "hAxis": {
-  //          "title": "Date"
-  //      }
-  //  };
-  //
-  //  chart1.formatters = {};
-  //
-  //  $scope.chart = chart1;
-
-
-
 
 });
