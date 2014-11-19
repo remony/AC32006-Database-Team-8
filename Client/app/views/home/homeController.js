8a08708839e635b34e0e5b0301f256b3cee822a6
remony <sudouglas@dundee.ac.uk>
2014-11-19 10:58:12 +0000

    more pretty stuff

--- Client/app/views/home/homeController.js
+++ Client/app/views/home/homeController.js
@@ -3,7 +3,17 @@ angular.module('app.homeController', [])
 
 .controller('homeController', function($scope, $http) {
   $scope.title = "Home";
-  $http({url: backend + '/', method:'GET',dataType:'json', headers: {
-  }}).success($scope.online=true);
-
+  $.ajax({
+          type: "get",
+          url: backend + "/",
+          //beforeSend: function (xhr) {xhr.setRequestHeader ("Authorization", $cookies.monster_cookie)},
+          }).done(function(data){
+            //something
+          }).fail(function(data){
+            console.log("service says boo");
+            false;
+          }).success(function(data){
+  			$scope.data = data;
+                $scope.online = true;
+          });
 });
